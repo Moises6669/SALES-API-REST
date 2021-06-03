1cbe332c016eb35d50e98a6738667f45db5ba0ba
Nobody <moisesalex132@gmail.com>
2021-06-03 10:46:23 -0600

    🛠 FIX: rutas arregladas

--- routes/api.php
+++ routes/api.php
@@ -58,9 +58,9 @@ Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['
 /**
  * Users
  */
-// Route::name('me')->get('users/me', 'User\UserController@me');
-// Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
-// Route::name('verify')->get('users/verify/{token}', 'User\UserController@verify');
-// Route::name('resend')->get('users/{user}/resend', 'User\UserController@resend');
+Route::name('me')->get('users/me', 'User\UserController@me');
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
+Route::name('verify')->get('users/verify/{token}', 'User\UserController@verify');
+Route::name('resend')->get('users/{user}/resend', 'User\UserController@resend');
 
-// Route::post('oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
+Route::post('oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
